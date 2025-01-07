7342e3ac5d8cffe864e95f65e5f397bb1c50ac21
Mathieu Hardy <mhardy2008@gmail.com>
2025-01-07 22:18:27 +0100

    Remove AI for now

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -1,17 +1,1 @@
-return {
-  -- Self-hosted AI server
-  {
-    enabled = os.getenv("BOOK") ~= "1",
-    "TabbyML/vim-tabby",
-    lazy = false,
-    dependencies = {
-      "neovim/nvim-lspconfig",
-    },
-    init = function()
-      vim.g.tabby_agent_start_command = { "npx", "tabby-agent", "--stdio" }
-      vim.g.tabby_inline_completion_trigger = "manual"
-      vim.g.tabby_inline_completion_keybinding_trigger_or_dismiss = "<C-h>"
-      vim.g.tabby_inline_completion_keybinding_accept = "<C-j>"
-    end,
-  },
-}
+return {}
