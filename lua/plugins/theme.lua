183f5ce9f785274d0661edeb6488c80770a880a2
Mathieu Hardy <mhardy2008@gmail.com>
2025-01-11 12:45:53 +0100

    Disable italic comments

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -6,6 +6,7 @@ return {
     lazy = false,
     init = function()
       vim.g.sonokai_style = "default"
+      vim.g.sonokai_disable_italic_comment = 1
       vim.cmd.colorscheme("sonokai")
     end,
   },
