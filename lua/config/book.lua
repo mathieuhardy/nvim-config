f75ca998b8c763d05290087fa059a9727da76081
Mathieu Hardy <mhardy2008@gmail.com>
2025-06-08 18:59:19 +0200

    feat: add new spell file for french

--- lua/config/book.lua
+++ lua/config/book.lua
@@ -1,3 +1,8 @@
+-- To generate the spell file:
+--   git clone https://github.com/thjbdvlt/spell-fr.vim.git
+--   make fr.utf-8.spl
+--   mv fr.utf-8.spl ~/.config/nvim/spell/
+
 if os.getenv("BOOK") == "1" then
   -- Wrapping
   vim.o.wrap = true
@@ -6,8 +11,9 @@ if os.getenv("BOOK") == "1" then
   vim.o.wrapmargin = 1
 
   -- Spelling
-  vim.o.spell = true
   vim.o.spelllang = "fr"
+  vim.opt.spellfile = vim.fn.getcwd() .. "/spell/fr.utf-8.add"
+  vim.o.spell = true
 
   -- Abbreviations
   vim.cmd("iabbrev -- —")
