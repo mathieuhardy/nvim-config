a07b71e1e6485882f7ec9021aed46d2dd7b84504
Mathieu Hardy <mhardy2008@gmail.com>
2025-06-30 13:33:06 +0200

    Disable RTP check

--- lua/plugins/interface.lua
+++ lua/plugins/interface.lua
@@ -151,6 +151,11 @@ return {
   {
     "OXY2DEV/markview.nvim",
     lazy = false,
+    opts = {
+      experimental = {
+        check_rtp = false,
+      },
+    },
   },
 
   -- Colors in help documentations
