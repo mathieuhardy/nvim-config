cf1d2da4204ce7f84c0a50acd0ddffd2ffd8ca55
Mathieu Hardy <mhardy2008@gmail.com>
2025-12-20 18:10:23 +0100

    Replace CSS colorization plugin

--- lua/plugins/highlighting.lua
+++ lua/plugins/highlighting.lua
@@ -1,14 +1,33 @@
 return {
   -- Highlight CSS colors
   {
-    "norcalli/nvim-colorizer.lua",
+    "catgoose/nvim-colorizer.lua",
     enabled = true,
     lazy = false,
-    init = function()
-      vim.opt.termguicolors = true
-    end,
+    event = "BufReadPre",
     config = function()
-      require("colorizer").setup()
+      vim.o.termguicolors = true
+
+      require("colorizer").setup({
+        filetypes = { "*" }, -- Active pour tous les filetypes
+        user_default_options = {
+          RGB = true, -- #RGB hex codes
+          RRGGBB = true, -- #RRGGBB hex codes
+          names = true, -- "Name" codes like Blue or blue
+          RRGGBBAA = true, -- #RRGGBBAA hex codes
+          AARRGGBB = true, -- 0xAARRGGBB hex codes
+          rgb_fn = true, -- CSS rgb() and rgba() functions
+          hsl_fn = true, -- CSS hsl() and hsla() functions
+          css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+          css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+          mode = "background", -- Set the display mode (foreground, background, virtualtext)
+          tailwind = true, -- Enable tailwind colors
+          sass = { enable = true, parsers = { "css" } },
+          virtualtext = "■",
+          always_update = false,
+        },
+        buftypes = {},
+      })
     end,
   },
 
