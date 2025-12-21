3cd535afce05d5b14827f4bec656b700eba3250d
Mathieu Hardy <mhardy2008@gmail.com>
2025-12-21 19:30:27 +0100

    Disable AI by default

--- lua/config/ai.lua
+++ lua/config/ai.lua
@@ -1,3 +1,1 @@
-if global_opts.ai.suggestions_backend ~= nil and global_opts.ai.suggestions_backend ~= "" then
-  select_ai_suggestions_backend(global_opts.ai.suggestions_backend)
-end
+select_ai_suggestions_backend(global_opts.ai.suggestions_backend)
